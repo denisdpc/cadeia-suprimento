2021c643f641032a04e01b8ff876a46338250cd6
denisdpc <denisdpc@gmail.com>
2019-04-02 08:31:10 -0300

    acerto variaveis

--- simu.R
+++ simu.R
@@ -2,18 +2,24 @@ library(simmer)
 library(simmer.plot)
 
 # levantamento estatistico
-FREQ_FALHA_BASE= 4 # frequencia de pane no ano
-ESFORCO_BASE = 485 # horas de utilização para a freq de panes
-TAT=15 # turn around time (meses)
+#FREQ_FALHA_BASE= 2.67 # media de tempo em meses por pane
+#ESFORCO_BASE = 508 # esforco medio anual
+TAT=11 # turn around time (meses)
 
 # configuracao
 ANVS = 2 # numero de aeronaves
-QPA = 2 # qtd de ites por aeronave
-GIRO = 5 # qtd de itens ativos
+QPA = 1 # qtd de ites por aeronave
+GIRO = 3 # qtd de itens ativos
 
 # esforco para simulacao
-ESFORCO = 485
-FREQ_FALHA = FREQ_FALHA_BASE*ESFORCO_BASE/ESFORCO
+#ESFORCO = 460
+#FREQ_FALHA = FREQ_FALHA_BASE*ESFORCO_BASE/ESFORCO
+
+ESFORCO_PERIODO = 1525  # esforco acumulado em um periodo
+FALHAS_PERIODO = 8      # falhas acumuladas em um periodo
+ESFORCO_PREVISTO = 460  # esforco previsto para simulacao
+
+PERIODICIDADE_FALHAS = 12*ESFORCO_PERIODO/ESFORCO_PREVISTO/FALHAS_PERIODO # intervalo entre falhas (em meses)
 
 aeronaves <- paste0("anv_",1:ANVS)
 
@@ -42,7 +48,7 @@ reparo <- trajectory() %>%
 # remanejado para "reparo"
 
 env %>% add_resource("item", GIRO,preemptive=TRUE) %>% # qtd de itens
-add_generator("reparo", reparo, function() FREQ_FALHA, priority=1) # freq de falhas em meses
+add_generator("reparo_", reparo, function() PERIODICIDADE_FALHAS, priority=1) # freq de falhas em meses
 
 for (anv in aeronaves) 
   env %>% add_generator(paste0(anv,"_aplicacao"), esforco(anv), at(0), restart = TRUE)
@@ -50,6 +56,8 @@ for (anv in aeronaves)
 
 env %>% reset() %>% run(30) # em meses
 
-#env %>% get_mon_resources() %>% head()
-#env %>% get_mon_arrivals() %>% head()
-#plot(get_mon_resources(env), metric = "usage", items="queue",steps=TRUE)
+env %>% get_mon_resources() %>% head()
+env %>% get_mon_arrivals() %>% head()
+plot(get_mon_resources(env), metric = "usage", items="queue",steps=TRUE)
+
+
